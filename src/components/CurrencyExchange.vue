c2a07069493fab78ee3e6a742d20f011f364be62
Arthur <arthur.mirzoyan@vocter.com>
2017-11-13 17:43:39 +0200

    add trim func

--- src/components/CurrencyExchange.vue
+++ src/components/CurrencyExchange.vue
@@ -5,17 +5,17 @@
 				<!-- form-group -->
 				<div class="form-group">
     			<label for="fsym">Currency 1</label>
-    				<input type="text" class="form-control" id="fsym" name="fsym" placeholder="Enter first currency" v-model="formInfo.fsym">
+    				<input type="text" class="form-control" id="fsym" name="fsym" placeholder="Enter first currency" v-model.trim="formInfo.fsym">
 				</div>
 				<!-- END:form-group -->
 
 				<!-- form-group -->
 				<div class="form-group">
     			<label for="tsyms">Currency 2</label>
-    				<input type="text" class="form-control" id="tsyms" name="tsyms" placeholder="Enter secnd currency" v-model="formInfo.tsyms">
+    				<input type="text" class="form-control" id="tsyms" name="tsyms" placeholder="Enter secnd currency" v-model.trim="formInfo.tsyms">
 				</div>
 				<!-- END:form-group -->
-				<button type="submit" class="form-submit btn btn-primary">Check Excnahge of the values</button>
+				<button type="submit" class="form-submit btn btn-primary">Show exchange Rate</button>
 			</form>
 			<!-- formOutput -->
 			<div id="formOutput" v-if="formResponse.message">
